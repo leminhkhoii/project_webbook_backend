4f7a26aa532ebe2daf617e63b09bb4073a39cb3f
khoi <leminhkhoi.cuchi@gmail.com>
2023-09-18 22:53:11 +0700

    Tao du an moi

---
+++ src/main/java/vn/sheldon/project_webbook_backend/ProjectWebbookBackendApplication.java
@@ -0,0 +1,13 @@
+package vn.sheldon.project_webbook_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWebbookBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWebbookBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/sheldon/project_webbook_backend/ProjectWebbookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.sheldon.project_webbook_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWebbookBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
